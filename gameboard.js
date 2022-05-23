bca1670786d2bf8e7c8382cbe8c7bf4db8237e82
Tom <87855471+Tom2612@users.noreply.github.com>
2022-05-23 16:45:53 +0800

    Update gameboard.js
    Added placeShip function

--- gameboard.js
+++ gameboard.js
@@ -1,19 +1,31 @@
 const Ship = require('./ship');
 
 const gameboard = () => {
+    let board = [];
     const makeBoard = () => {
-        let board = [];
-        for (let i = 1; i < 5; i++) {
+        for (let i = 1; i < 11; i++) {
             board.push(i);
         }
         return board;
     }
-    const placeShip = (name, length, location) => {
-        
-        return []
+    const placeShip = (location, length, name) => {
+        makeBoard();
+        if(location + length - 2 < board.length){
+            if(!name){
+                for (let j = location - 1; j < location + length - 1; j++) {
+                    board.splice(j, 1, 'name');
+                }
+                return board;
+            } else {
+                const ship = Ship(name, length);
+                for (let j = location - 1; j < location + length - 1; j++) {
+                    board.splice(j, 1, ship.getName());
+                }
+                return board;
+            }
+        } else return false;
     }
     return { makeBoard, placeShip };
 }
-
 
 module.exports = gameboard;

a81cc16284bba7612415366661e2a5059595374a
Tom <87855471+Tom2612@users.noreply.github.com>
2022-05-25 16:08:05 +0800

    Update helper.js
    Updated helper function

--- helper.js
+++ helper.js
@@ -7,21 +7,33 @@ function checkLocation(location, length) {
         i++;
     }
     console.log(array);
-    // split the number here and check the first digit of first number is same as first digit of last?
-    if ((location + length) % 10 !== 0) {
-        for (let i = 0; i < array.length; i++) { 
-            let number = array[i];
-            let nextNumber = array[i+1];
-            console.log(number, nextNumber)  
-            if(array[i] % 10 === 0){
-                if (nextNumber){
-                    result = false
-                } else {
-                    result = true;
-                } 
+
+    let a = array[0];
+    let b = array[array.length - 1];
+    console.log(a.toString().split(''), b.toString().split(''));
+    if(b > 10) {
+        if(a.toString().split('')[0] !== b.toString().split('')[0]) {
+            if(b.toString().split('')[1] !== 0){
+                return false;
             }
         }
-        return result;
     } else return true;
-    
 }
+    // split the number here and check the first digit of first number is same as first digit of last?
+    // if ((location + length) % 10 !== 0) {
+    //     for (let i = 0; i < array.length; i++) { 
+    //         let number = array[i];
+    //         let nextNumber = array[i+1];
+    //         console.log(number, nextNumber)  
+    //         if(array[i] % 10 === 0){
+    //             if (nextNumber){
+    //                 result = false
+    //             } else {
+    //                 result = true;
+    //             } 
+    //         }
+    //     }
+    //     return result;
+    // } else return true;
+    
+// }

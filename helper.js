69c7377d2baaa1d05ded6d4f8c796d755dd09c91
Tom <87855471+Tom2612@users.noreply.github.com>
2022-05-27 10:39:57 +0800

    Update helper.js
    Updated to check for vertical placement

--- helper.js
+++ helper.js
@@ -1,25 +1,36 @@
-function checkLocation(location, length) {
+function checkLocation(location, length, horizontal) {
     let i = location;
     let array = [];
-    while(array.length < length) {
-        array.push(i);
-        i++;
-    }
-    let a = array[0].toString().split('');
-    let b = array[array.length - 1].toString().split('');
-    if (a % 10 === 0 || a[0] === b[0]) {
+    if (horizontal) {
+        //Horizontal placement
+        while(array.length < length) {
+            array.push(i);
+            i++;
+        }
+        let a = array[0].toString().split('');
+        let b = array[array.length - 1].toString().split('');
+        if (a % 10 === 0 || a[0] === b[0]) {
+            return true;
+        }
+        if (b.length === 1) {
+            return true;
+        } 
+        if(location + length > 101) {
+            return false
+        }
+        if (b[1] !== '0') {
+            return false;
+        }
         return true;
+    } else {
+        //Vertical placement
+        while (array.length < length) {
+            array.push(i);
+            i += 10;
+        }
+        return (location + (length * 10) > 100 ? false : true);
     }
-    if (b.length === 1) {
-        return true;
-    } 
-    if(location + length > 101) {
-        return false
-    }
-    if (b[1] !== '0') {
-        return false;
-    }
-    return true;
+    
 }
 
 module.exports = checkLocation;

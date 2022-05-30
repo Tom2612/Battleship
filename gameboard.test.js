e3f4bd0f189f48bdf3aebef7b033b42d3096aced
Tom <87855471+Tom2612@users.noreply.github.com>
2022-05-30 16:54:26 +0800

    Update gameboard.test.js
    Added tests to check for occupied spaces, multiple boats and vertical placements

--- gameboard.test.js
+++ gameboard.test.js
@@ -15,22 +15,23 @@ test('place real ship and get name', () => {
 test('hit but don\'t sink real ship', () => {
     gameboard().makeBoard(11);
     const ship = Ship('sub', 3);
-    const returnValue = gameboard().placeShip(2, 3, ship.getName(), true)[1];
+    gameboard().placeShip(2, 3, ship.getName(), true)
+    const returnValue = gameboard().getBoard()[1];
     returnValue.hit(1)
     expect(returnValue.isSunk()).toBe(false);
 })
 
 test('hit and sink real ship', () => {
     gameboard().makeBoard(11);
-    const ship = Ship('sub', 3);
-    const returnValue = gameboard().placeShip(2, 1, ship.getName(), true)[1];
+    gameboard().placeShip(2, 1, 'boat', true)
+    const returnValue = gameboard().getBoard()[1];
     returnValue.hit(1)
     expect(returnValue.isSunk()).toBe(true);
 })
 test('hit and sink real ship at different locations', () => {
     gameboard().makeBoard(11);
-    const ship = Ship('sub', 3);
-    const returnValue = gameboard().placeShip(2, 2, ship.getName(), true)[1];
+    gameboard().placeShip(2, 2, 'boat2', true);
+    const returnValue = gameboard().getBoard()[1];
     returnValue.hit(1)
     returnValue.hit(2)
     expect(returnValue.isSunk()).toBe(true);
@@ -74,7 +75,7 @@ test('vertical placement test pass', () => {
     gameboard().makeBoard(21);
     const ship = Ship('patrol boat', 2);
     const returnValue = gameboard().placeShip(6, 2, ship.getName(), false)[15];
-    const returnValue2 = gameboard().placeShip(6, 2, ship.getName(), false)[5];
+    const returnValue2 = gameboard().getBoard()[5];
     expect(returnValue.getName()).toBe('patrol boat');
     expect(returnValue2.getName()).toBe('patrol boat');
 })
@@ -83,6 +84,17 @@ test('vertical placement test fail', () => {
     const ship = Ship('patrol boat', 2);
     const returnValue = gameboard().placeShip(96, 2, ship.getName(), false);
     expect(returnValue).toBe(false);
+})
+test('2 boats hit and sink 1', () => {
+    gameboard().makeBoard(101);
+    gameboard().placeShip(1, 2, 'patrol boat', false);
+    gameboard().placeShip(3, 3, 'submarine', true);
+    const boat = gameboard().getBoard()[0];
+    const boat2 = gameboard().getBoard()[2];
+    boat.hit(1);
+    boat.hit(11);
+    expect(boat.isSunk()).toBe(true);
+    expect(boat2.isSunk()).toBe(false);
 })
 
 //ReceiveAttack function tests

9f99ca1a26d306a293c2157fe91b4fb6d086b056
Tom <87855471+Tom2612@users.noreply.github.com>
2022-05-30 16:53:00 +0800

    Update checkvalid.js
    Added vertical checking

--- checkvalid.js
+++ checkvalid.js
@@ -1,23 +1,25 @@
 function checkOccupied(location, length, direction, board) {
     let i = location;
     let array = [];
+    let map = [];
     //Horizontal placement
     if (direction) {
         while(array.length < length) {
             array.push(i);
             i++;
         }
-        for (let locations of array) {
-            if(typeof board[locations - 1] !== 'number') {
-                return false;
-            } else return true;
-        }
+        //Vertical placement
     } else {
         while (array.length < length) {
             array.push(i);
             i += 10;
         }
     }
+    for (let place of array) {
+        if (typeof board[place - 1] !== 'number') {
+            return false;
+        } else map.push(typeof board[place - 1]);
+    } return map.length === array.length ? true : false;
 };
 
 module.exports = checkOccupied;

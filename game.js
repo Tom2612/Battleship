6fda3a7391d8b678b345499979cdfe7861a3fbec
Tom <87855471+Tom2612@users.noreply.github.com>
2022-07-05 11:46:22 +0800

    Update game.js
    Added practice hits, play round function and begun linking up with app.js

--- game.js
+++ game.js
@@ -3,8 +3,11 @@
 // const Player = require('./player');
 
 import Ship from './ship.js';
-import gameboard from './gameboard.js';
-import player from './player.js'
+import Gameboard from './gameboard.js';
+import Player from './player.js'
+
+const playerGrid = document.querySelector('.player');
+const computerGrid = document.querySelector('.computer');
 
 const boats = {
     1: {
@@ -30,8 +33,8 @@ const boats = {
 }
 
 const Game = () => {
-    const playerName = prompt('Enter your name!').value;
-    const player1 = Player(playerName);
+    // const playerName = prompt('Enter your name!').value;
+    const player1 = Player('playerName');
     const computerPlayer = Player('computer');
     
     const playerGameboard = Gameboard();
@@ -39,6 +42,7 @@ const Game = () => {
     playerGameboard.makeBoard(101);
     computerGameboard.makeBoard(101);
 
+    //Hard-coded locations for now
     playerGameboard.placeShip(1, boats[1].length, boats[1].name, true);
     playerGameboard.placeShip(10, boats[2].length, boats[2].name, false);
     playerGameboard.placeShip(71, boats[3].length, boats[3].name, false);
@@ -50,6 +54,56 @@ const Game = () => {
     computerGameboard.placeShip(91, boats[3].length, boats[3].name, true);
     computerGameboard.placeShip(34, boats[4].length, boats[4].name, false);
     computerGameboard.placeShip(98, boats[5].length, boats[5].name, true);
+
+    playerGameboard.receiveAttack(1)
+    playerGameboard.receiveAttack(2)
+    playerGameboard.receiveAttack(3)
+    playerGameboard.receiveAttack(4)
+    playerGameboard.receiveAttack(5)
+    playerGameboard.receiveAttack(6)
+    playerGameboard.receiveAttack(7)
+    playerGameboard.receiveAttack(56)
+    playerGameboard.receiveAttack(57)
+
+    // const playRound = (board, location) => {
+    //     if (!board.checkAllSunk()) {
+    //         board.receiveAttack(location);
+    //         computerPlayer.randomLocation(playerGameboard);
+    //     } else {
+    //         gameOver();
+    //     }
+    // }
+
+    const playRoundNew = (location) => {
+        computerGameboard.receiveAttack(location);
+        let result = computerGameboard.checkAllSunk();
+        if (result) {
+            gameOver('Player');
+        }
+        //For now, computer matches player.
+        playerGameboard.receiveAttack(location);
+        let computerResult = playerGameboard.checkAllSunk();
+        if (computerResult) {
+            gameOver('Computer');
+        }
+        return;
+    }
+
+    const gameOver = () => {
+
+    }
+
+    return {
+        playerGameboard,
+        computerGameboard,
+        playRoundNew,
+        getPlayerBoard: playerGameboard.getBoard,
+        getComputerBoard: computerGameboard.getBoard
+    }
 }
+
+let game = Game();
+console.log(game.getPlayerBoard())
+console.log(game.getComputerBoard())
 
 export default Game;

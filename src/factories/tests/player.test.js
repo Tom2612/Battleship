c833d3120bbd333a24b04ade058c9e7331bfd31d
Tom <87855471+Tom2612@users.noreply.github.com>
2022-07-13 12:10:11 +0800

    Update player.test.js

--- src/factories/tests/player.test.js
+++ src/factories/tests/player.test.js
@@ -1,11 +1,6 @@
-const Player = require('./player');
+const Player = require('../player');
 
 test('create player', () => {
     const player1 = Player('tom');
     expect(player1.getName()).toBe('tom');
-})
-
-test('getBoard', () => {
-    const player1 = Player('tom');
-    expect(player1.getBoard()[0]).toBe(1);
 })
